1fb4919aff3df854cdd92ed92c617c68454b5821
ryo0 <nekonekosekai@gmail.com>
2020-11-10 16:55:52 +0900

    feat: はじめのコード生成/テスト

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    let data = args[0].clone();
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", data);
+    println!("  ret");
 }
