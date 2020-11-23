bdc6bb7f44c274944e5d171344f38c9880782250
ryo0 <nekonekosekai@gmail.com>
2020-11-23 20:55:13 +0900

    とりあえず1個目のテストが通った

--- test.sh
+++ test.sh
@@ -111,8 +111,8 @@ assert() {
 
 
 assert 32 'int main() { return ret32(); } int ret32() { return 32; }'
-# assert 7 'int main() { return add2(3,4); } int add2(int x, int y) { return x+y; }'
-# assert 1 'int main() { return sub2(4,3); } int sub2(int x, int y) { return x-y; }'
-# assert 55 'int main() { return fib(9); } int fib(int x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }'
+assert 7 'int main() { return add2(3,4); } int add2(x, y) { return x+y; }'
+assert 1 'int main() { return sub2(4,3); } int sub2( x, i y) { return x-y; }'
+assert 55 'int main() { return fib(9); } int fib(x) { if (x<=1) return 1; return fib(x-1) + fib(x-2); }'
 
 echo OK
