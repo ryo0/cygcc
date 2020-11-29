6832cd290d973027baf3c49fc82bf45c4ac5ab2e
ryo0 <nekonekosekai@gmail.com>
2020-11-29 21:55:08 +0900

    add test

--- test.sh
+++ test.sh
@@ -132,4 +132,6 @@ assert 5 'int main() { x=3; y=5; return *(&x+-8); }'
 assert 7 'int main() { x=3; y=5; *(&x-8)=7; return y; }'
 assert 7 'int main() { x=3; y=5; *(&y+8)=7; return x; }'
 assert 5 'int main() { x=3; return (&x+2)-&x+3; }'
+
+assert 3 'int main() { x = 0; y=&x; *y = 3; return x; }'
 echo OK
